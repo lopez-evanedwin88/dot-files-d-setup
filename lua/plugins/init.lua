a2a5ee6779b57783fdf65d14172fdc4540a8c5b6
Evan Edwin Lopez <mac@macs-MacBook-Pro.local>
2024-05-09 17:26:25 +0800

    Change packer to lazy nvim

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,61 +1,32 @@
-local fn = vim.fn
-
--- Automatically install packer
-local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
-if fn.empty(fn.glob(install_path)) > 0 then
-    PACKER_BOOTSTRAP = fn.system({
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+    vim.fn.system({
         "git",
         "clone",
-        "--depth",
-        "1",
-        "https://github.com/wbthomason/packer.nvim",
-        install_path,
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
     })
-    print("Installing packer close and reopen Neovim...")
-    vim.cmd([[packadd packer.nvim]])
 end
+vim.opt.rtp:prepend(lazypath)
 
--- Autocommand that reloads neovim whenever you save the plugins.lua file
-vim.cmd([[
-  augroup packer_user_config
-    autocmd!
-    autocmd BufWritePost plugins.lua source <afile> | PackerSync
-  augroup end
-]])
-
--- Use a protected call so we don't error out on first use
-local status_ok, packer = pcall(require, "packer")
-if not status_ok then
-    return
-end
-
--- Have packer use a popup window
-packer.init({
-    display = {
-        open_fn = function()
-            return require("packer.util").float({ border = "rounded" })
-        end,
-    },
-})
-
-return packer.startup(function(use)
-    -- Packer manager
-    use 'wbthomason/packer.nvim'
-    use 'nvim-tree/nvim-web-devicons'
-    use { 'nvim-treesitter/nvim-treesitter', run = ":TSUpdate" }
-
-    use {
+require("lazy").setup({
+    { 'wbthomason/packer.nvim' },
+    { 'nvim-tree/nvim-web-devicons' },
+    { 'nvim-treesitter/nvim-treesitter', run = ":TSUpdate" },
+    {
         'nvim-lualine/lualine.nvim',
         requires = { 'nvim-tree/nvim-web-devicons', opt = true }
-    }
-    use { 'akinsho/bufferline.nvim', tag = "*", requires = 'nvim-tree/nvim-web-devicons' }
-    use {
+    },
+    { 'akinsho/bufferline.nvim', version = "*", requires = 'nvim-tree/nvim-web-devicons' },
+    {
         'nvim-tree/nvim-tree.lua',
         requires = {
             'nvim-tree/nvim-web-devicons', -- optional
         },
-    }
-    use {
+    },
+    {
         "folke/which-key.nvim",
         config = function()
             vim.o.timeout = true
@@ -66,15 +37,16 @@ return packer.startup(function(use)
                 -- refer to the configuration section below
             }
         end
-    }
-    use {
-        'nvim-telescope/telescope.nvim', tag = '0.1.6',
-        requires = { { 'nvim-lua/plenary.nvim' } }
-    }
-    use {
+    },
+    {
+        'nvim-telescope/telescope.nvim',
+        tag = '0.1.6',
+        dependencies = { 'nvim-lua/plenary.nvim' }
+    },
+    {
         'VonHeikemen/lsp-zero.nvim',
         branch = 'v3.x',
-        requires = {
+        dependencies = {
             -- LSP SUpport
             { 'neovim/nvim-lspconfig' },
             { 'williamboman/mason.nvim' },
@@ -91,39 +63,35 @@ return packer.startup(function(use)
             { 'L3MON4D3/LuaSnip' },
             { 'rafamadriz/friendly-snippets' },
         }
-    }
-    use {
+    },
+    {
         "benfowler/telescope-luasnip.nvim",
-    }
-    use {
+    },
+    {
         "jiaoshijie/undotree",
         requires = {
             "nvim-lua/plenary.nvim",
         },
-    }
-    use {
+    },
+    {
         "lewis6991/gitsigns.nvim",
         requires = {
             "nvim-lua/plenary.nvim",
         },
-    }
-    use { "akinsho/toggleterm.nvim", tag = '*', config = function()
-        require("toggleterm").setup()
-    end }
-    use({
+    },
+    {
+        "akinsho/toggleterm.nvim",
+        version = '*',
+        config = true
+    },
+    ({
         "kdheepak/lazygit.nvim",
         -- optional for floating window border decoration
         requires = {
             "nvim-lua/plenary.nvim",
         },
-    })
-
-
+    }),
     -- Themes
-    use "rebelot/kanagawa.nvim"
-    use 'shaunsingh/nord.nvim'
-
-    if PACKER_BOOTSTRAP then
-        require("packer").sync()
-    end
-end)
+    { "rebelot/kanagawa.nvim" },
+    { 'shaunsingh/nord.nvim' }
+})
