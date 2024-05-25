f646ac99c25bfa090abce4009ac24aa38ceeb4a8
Evan Edwin Lopez <mac@macs-MBP.bbrouter>
2024-05-26 01:02:40 +0800

    Update treesitter-config

--- lua/treesitter-config/init.lua
+++ lua/treesitter-config/init.lua
@@ -1,5 +1,11 @@
-require("nvim-treesitter.configs").setup({
-    ensure_installed = { "json",
+require("lazy").setup({ {
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  config = function()
+    local configs = require("nvim-treesitter.configs")
+
+    configs.setup({
+      ensure_installed = { "json",
         "javascript",
         "typescript",
         "yaml",
@@ -11,18 +17,20 @@ require("nvim-treesitter.configs").setup({
         "lua",
         "vim",
         "vue", },
-    sync_install = false,
-    auto_install = true,
-    highlight = {
+      sync_install = false,
+      auto_install = true,
+      highlight = {
         enable = true,
         additional_vim_regex_highlighting = true,
-    },
-    rainbow = {
+      },
+      rainbow = {
         enable = true,
         -- disable = { "jsx", "cpp" }, list of languages you want to disable the plugin for
         extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
         max_file_lines = nil, -- Do not enable for files with more than n lines, int
         -- colors = {}, -- table of hex strings
         -- termcolors = {} -- table of colour name strings
-    }
-})
+      }
+    })
+  end
+} })
