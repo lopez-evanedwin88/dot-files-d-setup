f1d711b4f9b9de3d9f92b7d45c71e93005fe0fb4
Evan Edwin Lopez <mac@macs-MBP.bbrouter>
2024-05-31 14:37:03 +0800

    Define servers in mason

--- lua/lsp/init.lua
+++ lua/lsp/init.lua
@@ -15,7 +15,7 @@ end)
 -- read this: https://github.com/VonHeikemen/lsp-zero.nvim/blob/v3.x/doc/md/guide/integrate-with-mason-nvim.md
 require('mason').setup({})
 require('mason-lspconfig').setup({
-  ensure_installed = {},
+  ensure_installed = { "lua_ls", "vuels", "tsserver", "tsserver" },
   handlers = {
     function(server_name)
       require('lspconfig')[server_name].setup({})
