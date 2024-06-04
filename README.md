e0d81b0e9f7481ff3690d0b6007db38e246a55de
Evan Edwin Lopez <mac@macs-MBP.bbrouter>
2024-06-04 22:49:45 +0800

    Change file diretory to include tmux

--- README.md
+++
@@ -1,1 +0,0 @@
-# neo-vim-d-ide
--- lua/bufferline-config/init.lua
+++
@@ -1,8 +0,0 @@
-
-vim.opt.termguicolors = true
-require("bufferline").setup({
-    options = {
-      mode = 'buffers',
-      offsets = {{filetype = "NvimTree", text = "File Explorer" , text_align = "center" }},
-    }
-})
