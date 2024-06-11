f0750f6b9d24ad806fc355daef75b1ace0e3dfca
Evan Edwin Lopez <lopez.evanedwin@gmail.com>
2024-06-11 13:19:04 +0800

    Modify config lualine

--- .config/nvim/lua/lualine-config/init.lua
+++ .config/nvim/lua/lualine-config/init.lua
@@ -1,13 +1,38 @@
-local auto_theme_custom = require('lualine.themes.auto')
-auto_theme_custom.normal.c.bg = 'none'
+local colors = {
+	blue = "#80a0ff",
+	cyan = "#79dac8",
+	black = "#080808",
+	white = "#c6c6c6",
+	red = "#ff5189",
+	violet = "#d183e8",
+	grey = "#303030",
+}
 
+local bubbles_theme = {
+	normal = {
+		a = { fg = colors.black, bg = colors.violet },
+		b = { fg = colors.white, bg = colors.grey },
+		c = { fg = colors.white },
+	},
+
+	insert = { a = { fg = colors.black, bg = colors.blue } },
+	visual = { a = { fg = colors.black, bg = colors.cyan } },
+	replace = { a = { fg = colors.black, bg = colors.red } },
+
+	inactive = {
+		a = { fg = colors.white, bg = colors.black },
+		b = { fg = colors.white, bg = colors.black },
+		c = { fg = colors.white },
+	},
+}
 require("lualine").setup({
 	options = {
 		icons_enabled = true,
-		theme = auto_theme_custom,
-		component_separators = { left = "", right = "" },
-		section_separators = { left = "", right = "" },
+		theme = bubbles_theme,
+		component_separators = "",
+		section_separators = { left = "", right = "" },
 		disabled_filetypes = {
+      'neo-tree',
 			statusline = {},
 			winbar = {},
 		},
@@ -21,7 +46,7 @@ require("lualine").setup({
 		},
 	},
 	sections = {
-		lualine_a = { "mode" },
+		lualine_a = { { "mode", separator = { left = "" }, right_padding = 2 } },
 		lualine_b = { "branch", "diff", "diagnostics" },
 		lualine_c = { "filename" },
 		lualine_x = { "encoding", "fileformat", "filetype" },
