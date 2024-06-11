15e08d20a7aca831cb196c5f6a78574065d97cb7
Evan Edwin Lopez <lopez.evanedwin@gmail.com>
2024-06-11 13:58:44 +0800

    Added icons neo-tree

--- .config/nvim/lua/neotree-config/init.lua
+++ .config/nvim/lua/neotree-config/init.lua
@@ -31,15 +31,15 @@ require("neo-tree").setup({
 			last_indent_marker = "└",
 			highlight = "NeoTreeIndentMarker",
 			-- expander config, needed for nesting files
-			with_expanders = nil, -- if nil and file nesting is enabled, will enable expanders
+			with_expanders = true, -- if nil and file nesting is enabled, will enable expanders
 			expander_collapsed = "",
 			expander_expanded = "",
 			expander_highlight = "NeoTreeExpander",
 		},
 		icon = {
-			folder_closed = "",
-			folder_open = "",
-			folder_empty = "󰜌",
+			folder_closed = "",
+			folder_open = "",
+			folder_empty = "",
 			-- The next two settings are only a fallback, if you use nvim-web-devicons and configure default icons there
 			-- then these will never be used.
 			default = "*",
@@ -58,7 +58,7 @@ require("neo-tree").setup({
 			symbols = {
 				-- Change type
 				added = "✚" , -- or "✚", but this is redundant info if you use git_status_colors on the name
-				modified = "", -- or "", but this is redundant info if you use git_status_colors on the name
+				modified = "", -- or "", but this is redundant info if you use git_status_colors on the name
 				deleted = "✖", -- this can only be used in the git_status source
 				renamed = "󰁕", -- this can only be used in the git_status source
 				-- Status type
