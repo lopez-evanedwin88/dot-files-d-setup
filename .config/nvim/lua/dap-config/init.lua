d4c4a7e81a515366872c9c13bcb69176088415dd
Evan Edwin Lopez <lopez.evanedwin@gmail.com>
2024-07-20 23:53:54 +0800

    Added dap-config(In progress)

---
+++ .config/nvim/lua/dap-config/init.lua
@@ -0,0 +1,237 @@
+local dap, dapui = require("dap"), require("dapui")
+local dap_utils = require("dap.utils")
+require("dapui").setup()
+
+-- Change to local venv project
+require("dap-python").setup("/Users/evanlopez/Development/visibility-dashboard/venv/bin/python")
+
+vim.keymap.set("n", "<F5>", function()
+	require("dap").continue()
+end, { desc = "Continue" })
+vim.keymap.set("n", "<F10>", function()
+	require("dap").step_over()
+end, { desc = "Step Over" })
+vim.keymap.set("n", "<F11>", function()
+	require("dap").step_into()
+end, { desc = "Step Into" })
+vim.keymap.set("n", "<F12>", function()
+	require("dap").step_out()
+end, { desc = "Step Out" })
+vim.keymap.set("n", "<Leader>b", function()
+	require("dap").toggle_breakpoint()
+end, { desc = "Toggle Breakpoint" })
+vim.keymap.set("n", "<Leader>B", function()
+	require("dap").set_breakpoint()
+end, { desc = "Set Breakpoint" })
+vim.keymap.set("n", "<Leader>lp", function()
+	require("dap").set_breakpoint(nil, nil, vim.fn.input("Log point message: "))
+end, { desc = "Breakpoint condition:" })
+vim.keymap.set("n", "<Leader>dr", function()
+	require("dap").repl.open()
+end, { desc = "Toggle repl" })
+vim.keymap.set("n", "<Leader>dl", function()
+	require("dap").run_last()
+end, { desc = "Run Last" })
+vim.keymap.set({ "n", "v" }, "<Leader>dh", function()
+	require("dap.ui.widgets").hover()
+end, { desc = "Dap UI hover" })
+vim.keymap.set({ "n", "v" }, "<Leader>dp", function()
+	require("dap.ui.widgets").preview()
+end, { desc = "Dap UI Preview" })
+vim.keymap.set("n", "<Leader>df", function()
+	local widgets = require("dap.ui.widgets")
+	widgets.centered_float(widgets.frames)
+end, { desc = "Dap UI centered float frames" })
+vim.keymap.set("n", "<Leader>ds", function()
+	local widgets = require("dap.ui.widgets")
+	widgets.centered_float(widgets.scopes)
+end, { desc = "Dap UI centered float scopes" })
+
+vim.keymap.set("n", "<Leader>du", function()
+	require("dapui").toggle({})
+end, { desc = "Dap UI" })
+vim.keymap.set({ "n", "v" }, "<Leader>de", function()
+	require("dapui").eval()
+end, { desc = "Eval" })
+
+dap.listeners.before.attach.dapui_config = function()
+	dapui.open()
+end
+dap.listeners.before.launch.dapui_config = function()
+	dapui.open()
+end
+dap.listeners.before.event_terminated.dapui_config = function()
+	dapui.close()
+end
+dap.listeners.before.event_exited.dapui_config = function()
+	dapui.close()
+end
+
+require("nvim-dap-virtual-text").setup({
+	enabled = true, -- enable this plugin (the default)
+	enabled_commands = true, -- create commands DapVirtualTextEnable, DapVirtualTextDisable, DapVirtualTextToggle, (DapVirtualTextForceRefresh for refreshing when debug adapter did not notify its termination)
+	highlight_changed_variables = true, -- highlight changed values with NvimDapVirtualTextChanged, else always NvimDapVirtualText
+	highlight_new_as_changed = false, -- highlight new variables in the same way as changed variables (if highlight_changed_variables)
+	show_stop_reason = true, -- show stop reason when stopped for exceptions
+	commented = false, -- prefix virtual text with comment string
+	only_first_definition = true, -- only show virtual text at first definition (if there are multiple)
+	all_references = false, -- show virtual text on all all references of the variable (not only definitions)
+	clear_on_continue = false, -- clear virtual text on "continue" (might cause flickering when stepping)
+	--- A callback that determines how a variable is displayed or whether it should be omitted
+	--- @param variable Variable https://microsoft.github.io/debug-adapter-protocol/specification#Types_Variable
+	--- @param buf number
+	--- @param stackframe dap.StackFrame https://microsoft.github.io/debug-adapter-protocol/specification#Types_StackFrame
+	--- @param node userdata tree-sitter node identified as variable definition of reference (see `:h tsnode`)
+	--- @param options nvim_dap_virtual_text_options Current options for nvim-dap-virtual-text
+	--- @return string|nil A text how the virtual text should be displayed or nil, if this variable shouldn't be displayed
+	display_callback = function(variable, buf, stackframe, node, options)
+		-- by default, strip out new line characters
+		if options.virt_text_pos == "inline" then
+			return " = " .. variable.value:gsub("%s+", " ")
+		else
+			return variable.name .. " = " .. variable.value:gsub("%s+", " ")
+		end
+	end,
+	-- position of virtual text, see `:h nvim_buf_set_extmark()`, default tries to inline the virtual text. Use 'eol' to set to end of line
+	virt_text_pos = vim.fn.has("nvim-0.10") == 1 and "inline" or "eol",
+
+	-- experimental features:
+	all_frames = false, -- show virtual text for all stack frames not only current. Only works for debugpy on my machine.
+	virt_lines = false, -- show virtual lines instead of virtual text (will flicker!)
+	virt_text_win_col = nil, -- position the virtual text at a fixed window column (starting from the first text column) ,
+	-- e.g. 80 to position at column 80, see `:h nvim_buf_set_extmark()`
+})
+
+require("dap-vscode-js").setup({
+	-- node_path = "node", -- Path of node executable. Defaults to $NODE_PATH, and then "node"
+	debugger_path = vim.fn.resolve(vim.fn.stdpath("data") .. "/lazy/vscode-js-debug"),
+	-- debugger_cmd = { "js-debug-adapter" }, -- Command to use to launch the debug server. Takes precedence over `node_path` and `debugger_path`.
+	adapters = {
+		"chrome",
+		"pwa-node",
+		"pwa-chrome",
+		"pwa-msedge",
+		"pwa-extensionHost",
+		"node-terminal",
+	}, -- which adapters to register in nvim-dap
+	-- log_file_path = "(stdpath cache)/dap_vscode_js.log" -- Path for file logging
+	-- log_file_level = false -- Logging level for output to file. Set to false to disable file logging.
+	-- log_console_level = vim.log.levels.ERROR -- Logging level for output to console. Set to false to disable console output.
+})
+
+for _, language in ipairs({ "typescript", "javascript", "typescriptreact", "javascriptreact", "vue" }) do
+	dap.configurations[language] = {
+		{
+			type = "pwa-node",
+			request = "launch",
+			name = "Launch Current File (pwa-node)",
+			cwd = vim.fn.getcwd(),
+			args = { "${file}" },
+			sourceMaps = true,
+			protocol = "inspector",
+		},
+		{
+			type = "pwa-node",
+			request = "launch",
+			name = "Launch Current File (pwa-node with ts-node)",
+			cwd = vim.fn.getcwd(),
+			runtimeArgs = { "--loader", "ts-node/esm" },
+			runtimeExecutable = "node",
+			args = { "${file}" },
+			sourceMaps = true,
+			protocol = "inspector",
+			skipFiles = { "<node_internals>/**", "node_modules/**" },
+			resolveSourceMapLocations = {
+				"${workspaceFolder}/**",
+				"!**/node_modules/**",
+			},
+		},
+		{
+			type = "pwa-node",
+			request = "launch",
+			name = "Launch Current File (pwa-node with deno)",
+			cwd = vim.fn.getcwd(),
+			runtimeArgs = { "run", "--inspect-brk", "--allow-all", "${file}" },
+			runtimeExecutable = "deno",
+			attachSimplePort = 9229,
+		},
+		{
+			type = "pwa-node",
+			request = "launch",
+			name = "Launch Test Current File (pwa-node with jest)",
+			cwd = vim.fn.getcwd(),
+			runtimeArgs = { "${workspaceFolder}/node_modules/.bin/jest" },
+			runtimeExecutable = "node",
+			args = { "${file}", "--coverage", "false" },
+			rootPath = "${workspaceFolder}",
+			sourceMaps = true,
+			console = "integratedTerminal",
+			internalConsoleOptions = "neverOpen",
+			skipFiles = { "<node_internals>/**", "node_modules/**" },
+		},
+		{
+			type = "pwa-node",
+			request = "launch",
+			name = "Launch Test Current File (pwa-node with vitest)",
+			cwd = vim.fn.getcwd(),
+			program = "${workspaceFolder}/node_modules/vitest/vitest.mjs",
+			args = { "--inspect-brk", "--threads", "false", "run", "${file}" },
+			autoAttachChildProcesses = true,
+			smartStep = true,
+			console = "integratedTerminal",
+			skipFiles = { "<node_internals>/**", "node_modules/**" },
+		},
+		{
+			type = "pwa-node",
+			request = "launch",
+			name = "Launch Test Current File (pwa-node with deno)",
+			cwd = vim.fn.getcwd(),
+			runtimeArgs = { "test", "--inspect-brk", "--allow-all", "${file}" },
+			runtimeExecutable = "deno",
+			smartStep = true,
+			console = "integratedTerminal",
+			attachSimplePort = 9229,
+		},
+		{
+			type = "pwa-chrome",
+			request = "attach",
+			name = "Attach Program (pwa-chrome, select port)",
+			program = "${file}",
+			cwd = vim.fn.getcwd(),
+			sourceMaps = true,
+			port = function()
+				return vim.fn.input("Select port: ", 9222)
+			end,
+			webRoot = "${workspaceFolder}",
+		},
+		-- {
+		--   type = "node2",
+		--   request = "attach",
+		--   name = "Attach Program (Node2)",
+		--   processId = dap_utils.pick_process,
+		-- },
+		-- {
+		--   type = "node2",
+		--   request = "attach",
+		--   name = "Attach Program (Node2 with ts-node)",
+		--   cwd = vim.fn.getcwd(),
+		--   sourceMaps = true,
+		--   skipFiles = { "<node_internals>/**" },
+		--   port = 9229,
+		-- },
+		{
+			type = "pwa-node",
+			request = "attach",
+			name = "Attach Program (pwa-node, select pid)",
+			cwd = vim.fn.getcwd(),
+			processId = dap_utils.pick_process,
+			skipFiles = { "<node_internals>/**" },
+		},
+		-- Divider for the launch.json derived configs
+		{
+			name = "----- ↓ launch.json configs ↓ -----",
+			type = "",
+			request = "launch",
+		},
+	}
+end
--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -198,4 +198,25 @@ require("lazy").setup({
 	{
 		"folke/flash.nvim",
 	},
+	{
+		-- debugger adapter plugin manager
+		"jay-babu/mason-nvim-dap.nvim",
+	},
+	{
+		"mfussenegger/nvim-dap",
+		dependencies = {
+			"nvim-neotest/nvim-nio",
+			"rcarriga/nvim-dap-ui",
+			"theHamsta/nvim-dap-virtual-text",
+			-- debugger adapter plugins
+			"mxsdev/nvim-dap-vscode-js",
+			"mfussenegger/nvim-dap-python",
+		},
+	},
+	{
+		"microsoft/vscode-js-debug",
+		-- After install, build it and rename the dist directory to out
+		build = "npm install --legacy-peer-deps --no-save && npx gulp vsDebugServerBundle && rm -rf out && mv dist out",
+		version = "1.*",
+	},
 })
