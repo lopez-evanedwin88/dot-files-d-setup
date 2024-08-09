c4036ae2b0af2a0508446ce7479e34260b19f007
Evan Edwin Lopez <lopez.evanedwin@gmail.com>
2024-08-10 00:42:40 +0800

    Update surround keymaps

--- .config/nvim/lua/nvim-surround-config/init.lua
+++ .config/nvim/lua/nvim-surround-config/init.lua
@@ -1,1 +1,14 @@
-require("nvim-surround").setup({})
+require("nvim-surround").setup({
+	keymaps = { -- vim-surround style keymaps
+		insert = "<C-g>s",
+		insert_line = "<C-g>S",
+		normal = "s",
+		normal_cur = "ss",
+		normal_line = "S",
+		normal_cur_line = "SS",
+		visual = "s",
+		visual_line = "gS",
+		delete = "ds",
+		change = "cs",
+	},
+})
