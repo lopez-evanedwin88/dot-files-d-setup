efe7bb4854f43ac3faedd02a70334bc48df95950
Evan Edwin Lopez <lopez.evanedwin@gmail.com>
2024-08-09 13:30:44 +0800

    Added new icons for gitsigns and diagnostics

--- .config/nvim/lua/gitsigns-config/init.lua
+++ .config/nvim/lua/gitsigns-config/init.lua
@@ -1,15 +1,17 @@
+local icons = require("icons")
+
 require("gitsigns").setup({
   signs = {
-    add = { text = "+" },
-    change = { text = "~" },
-    delete = { text = "_" },
-    topdelete = { text = "‾" },
-    changedelete = { text = "~" },
-    untracked = { text = "┆" },
+    add = { text = icons.ui.BoldLineMiddle },
+    change = { text = icons.ui.BoldLineDashedMiddle },
+    delete = { text = icons.ui.TriangleShortArrowRight },
+    topdelete = { text = icons.ui.TriangleShortArrowRight },
+    changedelete = { text = icons.ui.BoldLineMiddle },
+    untracked = { text = icons.ui.BoldLineDashedMiddle },
   },
   signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
-  numhl = false,    -- Toggle with `:Gitsigns toggle_numhl`
-  linehl = false,   -- Toggle with `:Gitsigns toggle_linehl`
+  numhl = false,     -- Toggle with `:Gitsigns toggle_numhl`
+  linehl = false,    -- Toggle with `:Gitsigns toggle_linehl`
   word_diff = false, -- Toggle with `:Gitsigns toggle_word_diff`
   watch_gitdir = {
     follow_files = true,
@@ -29,7 +31,7 @@ require("gitsigns").setup({
   },
   sign_priority = 6,
   update_debounce = 100,
-  status_formatter = nil, -- Use default
+  status_formatter = nil,  -- Use default
   max_file_length = 40000, -- Disable if file is longer than this (in lines)
   preview_config = {
     -- Options passed to nvim_open_win
--- .config/nvim/lua/lsp/init.lua
+++ .config/nvim/lua/lsp/init.lua
@@ -1,4 +1,29 @@
 -- lsp-config.lua
+local icons = require("icons")
+vim.diagnostic.config({
+	signs = {
+		text = {
+			[vim.diagnostic.severity.ERROR] = icons.diagnostics.Error,
+			[vim.diagnostic.severity.WARN] = icons.diagnostics.Warning,
+			[vim.diagnostic.severity.HINT] = icons.diagnostics.Hint,
+			[vim.diagnostic.severity.INFO] = icons.diagnostics.Information,
+		},
+	},
+	virtual_text = true,
+	update_in_insert = false,
+	underline = true,
+	severity_sort = true,
+	float = {
+		focusable = true,
+		style = "minimal",
+		border = "rounded",
+		header = "",
+		prefix = "",
+	},
+})
+
+vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded" })
+vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = "rounded" })
 
 local lsp_zero = require("lsp-zero")
 lsp_zero.on_attach(function(_, bufnr)
@@ -9,7 +34,7 @@ lsp_zero.on_attach(function(_, bufnr)
 		preserve_mappings = false,
 	})
 
-  -- Added custom keybindings for LSP
+	-- Added custom keybindings for LSP
 	vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, { buffer = bufnr })
 end)
 
