2cbf9e86718221f5aae2bad8badcbec1d43a3d87
Evan Edwin Lopez <lopez.evanedwin@gmail.com>
2024-08-19 10:21:45 +0800

    Update noice config

--- .config/nvim/lua/noice-config/init.lua
+++ .config/nvim/lua/noice-config/init.lua
@@ -68,6 +68,12 @@ require("noice").setup({
 			view = "mini",
 		},
 	},
+	presets = {
+		bottom_search = true, -- use a classic bottom cmdline for search
+		long_message_to_split = true, -- long messages will be sent to a split
+		inc_rename = false, -- enables an input dialog for inc-rename.nvim
+		lsp_doc_border = false, -- add a border to hover docs and signature help
+	},
 })
 
 vim.keymap.set("c", "<S-Enter>", function()
