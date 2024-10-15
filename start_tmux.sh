f26ac368c4c502a1b806ad48d3af41963115ad11
Evan Edwin Lopez <lopez.evanedwin@gmail.com>
2024-10-15 19:00:57 +0800

    Update tmux start script

--- start_tmux.sh
+++ start_tmux.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
-# Create a new tmux session named 'workspace'
-tmux new-session -d -s workspace 'zsh'  # First window with Zsh (named 'workspace')
+
+# Set the session name to '~'
+SESSION_NAME="|"
+
+# Create a new tmux session named '~' and start with Zsh
+tmux new-session -d -s "$SESSION_NAME" 'zsh'  # First window with Zsh
+
 # Rename the first window to 'workspace'
-tmux rename-window 'workspace'  # Rename the first window
-# Create a second window named 'cmd' running Zsh
-tmux new-window -t workspace:1 'zsh'  # Second window
-tmux rename-window 'cmd'  # Rename the second window to 'cmd'
+tmux rename-window -t "$SESSION_NAME:0" 'workspace'  # Rename the first window
+
+# Create a second window running Zsh
+tmux new-window -t "$SESSION_NAME:1" 'zsh'  # Second window
+
+# Rename the second window to 'cmd'
+tmux rename-window -t "$SESSION_NAME:1" 'cmd'  # Rename the second window
+
 # Attach to the session
-tmux attach -t workspace  # Attach to the session
+tmux attach -t "$SESSION_NAME"  # Attach to the session
