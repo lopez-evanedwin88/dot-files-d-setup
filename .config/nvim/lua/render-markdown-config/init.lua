91af652459ae70106246c54b28471d2fa4b973f8
Evan Edwin Lopez <lopez.evanedwin@gmail.com>
2025-04-30 11:39:24 +0800

    Update render-markdown to disable anti_conceal

--- .config/nvim/lua/render-markdown-config/init.lua
+++ .config/nvim/lua/render-markdown-config/init.lua
@@ -1,5 +1,9 @@
 require("render-markdown").setup({
-  code = {
-    width = "block",
-  },
+	code = {
+		width = "block",
+	},
+	anti_conceal = {
+		-- This enables hiding any added text on the line the cursor is on.
+		enabled = false,
+	},
 })
