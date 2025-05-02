1259d4ddf90d68cee9a1c3369455332d6ff4311e
Evan Edwin Lopez <lopez.evanedwin@gmail.com>
2025-05-02 13:55:50 +0800

    Added render & preview md and also some shits plugins

--- .config/nvim/lua/custom/plugins/init.lua
+++ .config/nvim/lua/custom/plugins/init.lua
@@ -2,4 +2,29 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  {
+    'andymass/vim-matchup',
+    setup = function()
+      vim.g.matchup_matchparen_offscreen = { method = nil }
+      vim.g.matchup_matchpref = { html = { nolists = 1 } }
+    end,
+  },
+  {
+    'mg979/vim-visual-multi',
+  },
+  {
+    'mistweaverco/kulala.nvim',
+    ft = { 'http', 'rest' },
+    opts = {
+      -- your configuration comes here
+      global_keymaps = true,
+    },
+  },
+  {
+    'norcalli/nvim-colorizer.lua',
+    config = function()
+      require('colorizer').setup { '*' }
+    end,
+  },
+}
---
+++ .config/nvim/lua/custom/plugins/render-preview-md.lua
@@ -0,0 +1,24 @@
+return {
+  {
+    'MeanderingProgrammer/render-markdown.nvim',
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
+    opts = {
+      code = {
+        width = 'block',
+      },
+      anti_conceal = {
+        -- This enables hiding any added text on the line the cursor is on.
+        enabled = false,
+      },
+    },
+  },
+  {
+    'iamcco/markdown-preview.nvim',
+    cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
+    build = 'cd app && npm install',
+    init = function()
+      vim.g.mkdp_filetypes = { 'markdown' }
+    end,
+    ft = { 'markdown' },
+  },
+}
