19d79f25b66d10b875601c1fcdba0a6e985a61f7
Evan Edwin Lopez <lopez.evanedwin@gmail.com>
2025-05-03 22:51:00 +0800

    Update lsp-zero keymaps & enable format on save

--- .config/nvim/lua/lsp/init.lua
+++ .config/nvim/lua/lsp/init.lua
@@ -38,13 +38,46 @@ lsp_zero.on_attach(function(_, bufnr)
 	vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, { buffer = bufnr })
 end)
 
+local buffer_autoformat = function(bufnr)
+	local group = "lsp_autoformat"
+	vim.api.nvim_create_augroup(group, { clear = false })
+	vim.api.nvim_clear_autocmds({ group = group, buffer = bufnr })
+
+	vim.api.nvim_create_autocmd("BufWritePre", {
+		buffer = bufnr,
+		group = group,
+		desc = "LSP format on save",
+		callback = function()
+			-- note: do not enable async formatting
+			vim.lsp.buf.format({ async = false, timeout_ms = 10000 })
+		end,
+	})
+end
+
 -- https://lsp-zero.netlify.app/docs/language-server-configuration.html#disable-semantic-highlights
 vim.api.nvim_create_autocmd("LspAttach", {
 	callback = function(event)
+		local opts = { buffer = event.buf }
+
+		vim.keymap.set("n", "K", "<cmd>lua vim.lsp.buf.hover()<cr>", opts)
+		vim.keymap.set("n", "grd", "<cmd>lua vim.lsp.buf.definition()<cr>", opts)
+		vim.keymap.set("n", "grD", "<cmd>lua vim.lsp.buf.declaration()<cr>", opts)
+		vim.keymap.set("n", "gri", "<cmd>lua vim.lsp.buf.implementation()<cr>", opts)
+		vim.keymap.set("n", "gro", "<cmd>lua vim.lsp.buf.type_definition()<cr>", opts)
+		vim.keymap.set("n", "grr", "<cmd>lua vim.lsp.buf.references()<cr>", opts)
+		vim.keymap.set("n", "grs", "<cmd>lua vim.lsp.buf.signature_help()<cr>", opts)
+		vim.keymap.set("n", "grn", "<cmd>lua vim.lsp.buf.rename()<cr>", opts)
+		-- vim.keymap.set({ "n", "x" }, "<F3>", "<cmd>lua vim.lsp.buf.format({async = true})<cr>", opts)
+		vim.keymap.set("n", "gra", "<cmd>lua vim.lsp.buf.code_action()<cr>", opts)
 		local id = vim.tbl_get(event, "data", "client_id")
 		local client = id and vim.lsp.get_client_by_id(id)
 		if client == nil then
 			return
+		end
+
+		-- make sure there is at least one client with formatting capabilities
+		if client.supports_method("textDocument/formatting") then
+			buffer_autoformat(event.buf)
 		end
 
 		-- Disable semantic highlights
