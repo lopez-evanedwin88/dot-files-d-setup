4016224689ab8fa27777c9a22d448e8650822906
Evan Edwin Lopez <lopez.evanedwin@gmail.com>
2025-05-10 21:24:30 +0800

    Change config structure...

--- .config/nvim/lua/harpoon-config/init.lua
+++
@@ -1,7 +0,0 @@
-require("harpoon").setup({})
-
-vim.keymap.set("n", "hx", require("harpoon.mark").add_file, { desc = "Add harpoon file" })
-vim.keymap.set("n", "hn", require("harpoon.ui").nav_next, { desc = "Harpoon next buffer" })
-vim.keymap.set("n", "hp", require("harpoon.ui").nav_prev, { desc = "Harpoon prev buffer" })
-vim.keymap.set("n", "ho", require("harpoon.ui").toggle_quick_menu, { desc = "Open harpoon quick menu" })
-vim.keymap.set("n", "hm", ":Telescope harpoon marks<CR>", { desc = "Telescope open harpoon window" })
--- .config/nvim/lua/render-markdown-config/init.lua
+++
@@ -1,9 +0,0 @@
-require("render-markdown").setup({
-	code = {
-		width = "block",
-	},
-	anti_conceal = {
-		-- This enables hiding any added text on the line the cursor is on.
-		enabled = false,
-	},
-})
