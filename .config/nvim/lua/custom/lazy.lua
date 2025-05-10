e35c29756909209453ebd692d950e15904268043
Evan Edwin Lopez <lopez.evanedwin@gmail.com>
2025-05-10 22:44:12 +0800

    Update config

--- .config/nvim/lua/custom/lazy.lua
+++ .config/nvim/lua/custom/lazy.lua
@@ -1,164 +1,160 @@
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-	if vim.v.shell_error ~= 0 then
-		error("Error cloning lazy.nvim:\n" .. out)
-	end
+  local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
+  local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
+  if vim.v.shell_error ~= 0 then
+    error('Error cloning lazy.nvim:\n' .. out)
+  end
 end ---@diagnostic disable-next-line: undefined-field
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup({
-	{ import = "custom.plugins" },
-	{
-		"nvim-treesitter/nvim-treesitter-textobjects",
-		dependencies = { "nvim-treesitter/nvim-treesitter" },
-	},
-	{
-		"kdheepak/lazygit.nvim",
-		-- optional for floating window border decoration
-		cmd = {
-			"LazyGit",
-			"LazyGitConfig",
-			"LazyGitCurrentFile",
-			"LazyGitFilter",
-			"LazyGitFilterCurrentFile",
-		},
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-		},
-	},
-	{
-		"folke/noice.nvim",
-		lazy = false,
-		dependencies = {
-			"MunifTanjim/nui.nvim",
-			"rcarriga/nvim-notify",
-		},
-	},
-	{ "nvimdev/lspsaga.nvim" },
-	{
-		"ThePrimeagen/harpoon",
-		dependencies = { "nvim-lua/plenary.nvim" },
-	},
-	{ "JoosepAlviste/nvim-ts-context-commentstring" },
-	{
-		"0x100101/lab.nvim",
-		-- cmd = "cd js && npm ci", -- or install if manually
-		dependencies = { "nvim-lua/plenary.nvim" },
-	},
-	{
-		-- debugger adapter plugin manager
-		"jay-babu/mason-nvim-dap.nvim",
-	},
-	{
-		"mfussenegger/nvim-dap",
-		dependencies = {
-			"nvim-neotest/nvim-nio",
-			"rcarriga/nvim-dap-ui",
-			"theHamsta/nvim-dap-virtual-text",
-			-- debugger adapter plugins
-			"mxsdev/nvim-dap-vscode-js",
-			"mfussenegger/nvim-dap-python",
-		},
-	},
-	{
-		"microsoft/vscode-js-debug",
-		-- After install, build it and rename the dist directory to out
-		build = "npm install --legacy-peer-deps --no-save && npx gulp vsDebugServerBundle && rm -rf out && mv dist out",
-		version = "1.*",
-	},
-	{
-		"andymass/vim-matchup",
-		setup = function()
-			vim.g.matchup_matchparen_offscreen = { method = nil }
-			vim.g.matchup_matchpref = { html = { nolists = 1 } }
-		end,
-	},
-	-- {
-	-- 	"karb94/neoscroll.nvim",
-	-- 	commit = "e786577",
-	-- },
-	{
-		"folke/persistence.nvim",
-		event = "BufReadPre",
-		opts = { options = { "buffers", "curdir", "tabpages", "winsize", "help", "globals", "skiprtp" } },
-	},
-	{
-		"Exafunction/codeium.vim",
-		event = "BufEnter",
-		config = function()
-			-- Change '<C-g>' here to any keycode you like.
-			vim.keymap.set("i", "<C-g>", function()
-				return vim.fn["codeium#Accept"]()
-			end, { expr = true, silent = true })
-			vim.keymap.set("i", "<c-;>", function()
-				return vim.fn["codeium#CycleCompletions"](1)
-			end, { expr = true, silent = true })
-			vim.keymap.set("i", "<c-,>", function()
-				return vim.fn["codeium#CycleCompletions"](-1)
-			end, { expr = true, silent = true })
-			vim.keymap.set("i", "<c-x>", function()
-				return vim.fn["codeium#Clear"]()
-			end, { expr = true, silent = true })
-		end,
-	},
-	-- {
-	--   "brenoprata11/nvim-highlight-colors",
-	-- },
-	{
-		"MeanderingProgrammer/render-markdown.nvim",
-		dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.nvim" }, -- if you use the mini.nvim suite
-	},
-	{
-		"mg979/vim-visual-multi",
-	},
-	{
-		"kristijanhusak/vim-dadbod-ui",
-		dependencies = {
-			{ "tpope/vim-dadbod", lazy = true },
-			{ "kristijanhusak/vim-dadbod-completion", ft = { "sql", "mysql", "plsql" }, lazy = true }, -- Optional
-		},
-		cmd = {
-			"DBUI",
-			"DBUIToggle",
-			"DBUIAddConnection",
-			"DBUIFindBuffer",
-		},
-		init = function()
-			-- Your DBUI configuration
-			vim.g.db_ui_use_nerd_fonts = 1
-			vim.g.db_ui_save_location = "~/.config/nvim/db_connections.json" -- Save location
-			-- vim.g.db_ui_win_position = "right"
-			vim.g.db_ui_win_width = 100 -- Set width in columns
-		end,
-	},
-	{
-		"echasnovski/mini.indentscope",
-	},
-	{
-		"GitMarkedDan/you-are-an-idiot.nvim",
-	},
-	{
-		"nvim-treesitter/nvim-treesitter-context",
-		dependencies = { "nvim-treesitter/nvim-treesitter" },
-		event = "BufReadPost", -- Load after opening a buffer
-	},
-	{
-		"mistweaverco/kulala.nvim",
-		ft = { "http", "rest" },
-		opts = {
-			-- your configuration comes here
-			global_keymaps = true,
-		},
-	},
-	{
-		"iamcco/markdown-preview.nvim",
-		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-		build = "cd app && npm install",
-		init = function()
-			vim.g.mkdp_filetypes = { "markdown" }
-		end,
-		ft = { "markdown" },
-	},
-})
+require('lazy').setup {
+  { import = 'custom.plugins' },
+  {
+    'nvim-treesitter/nvim-treesitter-textobjects',
+    dependencies = { 'nvim-treesitter/nvim-treesitter' },
+  },
+  {
+    'kdheepak/lazygit.nvim',
+    -- optional for floating window border decoration
+    cmd = {
+      'LazyGit',
+      'LazyGitConfig',
+      'LazyGitCurrentFile',
+      'LazyGitFilter',
+      'LazyGitFilterCurrentFile',
+    },
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+    },
+  },
+  {
+    'folke/noice.nvim',
+    lazy = false,
+    dependencies = {
+      'MunifTanjim/nui.nvim',
+      'rcarriga/nvim-notify',
+    },
+  },
+  {
+    'ThePrimeagen/harpoon',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+  },
+  { 'JoosepAlviste/nvim-ts-context-commentstring' },
+  {
+    '0x100101/lab.nvim',
+    -- cmd = "cd js && npm ci", -- or install if manually
+    dependencies = { 'nvim-lua/plenary.nvim' },
+  },
+  {
+    -- debugger adapter plugin manager
+    'jay-babu/mason-nvim-dap.nvim',
+  },
+  {
+    'mfussenegger/nvim-dap',
+    dependencies = {
+      'nvim-neotest/nvim-nio',
+      'rcarriga/nvim-dap-ui',
+      'theHamsta/nvim-dap-virtual-text',
+      -- debugger adapter plugins
+      'mxsdev/nvim-dap-vscode-js',
+      'mfussenegger/nvim-dap-python',
+    },
+  },
+  {
+    'microsoft/vscode-js-debug',
+    -- After install, build it and rename the dist directory to out
+    build = 'npm install --legacy-peer-deps --no-save && npx gulp vsDebugServerBundle && rm -rf out && mv dist out',
+    version = '1.*',
+  },
+  {
+    'andymass/vim-matchup',
+    setup = function()
+      vim.g.matchup_matchparen_offscreen = { method = nil }
+      vim.g.matchup_matchpref = { html = { nolists = 1 } }
+    end,
+  },
+  -- {
+  -- 	"karb94/neoscroll.nvim",
+  -- 	commit = "e786577",
+  -- },
+  {
+    'folke/persistence.nvim',
+    event = 'BufReadPre',
+    opts = { options = { 'buffers', 'curdir', 'tabpages', 'winsize', 'help', 'globals', 'skiprtp' } },
+  },
+  {
+    'Exafunction/codeium.vim',
+    event = 'BufEnter',
+    config = function()
+      -- Change '<C-g>' here to any keycode you like.
+      vim.keymap.set('i', '<C-g>', function()
+        return vim.fn['codeium#Accept']()
+      end, { expr = true, silent = true })
+      vim.keymap.set('i', '<c-;>', function()
+        return vim.fn['codeium#CycleCompletions'](1)
+      end, { expr = true, silent = true })
+      vim.keymap.set('i', '<c-,>', function()
+        return vim.fn['codeium#CycleCompletions'](-1)
+      end, { expr = true, silent = true })
+      vim.keymap.set('i', '<c-x>', function()
+        return vim.fn['codeium#Clear']()
+      end, { expr = true, silent = true })
+    end,
+  },
+  -- {
+  --   "brenoprata11/nvim-highlight-colors",
+  -- },
+  {
+    'MeanderingProgrammer/render-markdown.nvim',
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
+  },
+  {
+    'mg979/vim-visual-multi',
+  },
+  {
+    'kristijanhusak/vim-dadbod-ui',
+    dependencies = {
+      { 'tpope/vim-dadbod', lazy = true },
+      { 'kristijanhusak/vim-dadbod-completion', ft = { 'sql', 'mysql', 'plsql' }, lazy = true }, -- Optional
+    },
+    cmd = {
+      'DBUI',
+      'DBUIToggle',
+      'DBUIAddConnection',
+      'DBUIFindBuffer',
+    },
+    init = function()
+      -- Your DBUI configuration
+      vim.g.db_ui_use_nerd_fonts = 1
+      vim.g.db_ui_save_location = '~/.config/nvim/db_connections.json' -- Save location
+      -- vim.g.db_ui_win_position = "right"
+      vim.g.db_ui_win_width = 100 -- Set width in columns
+    end,
+  },
+  {
+    'GitMarkedDan/you-are-an-idiot.nvim',
+  },
+  {
+    'nvim-treesitter/nvim-treesitter-context',
+    dependencies = { 'nvim-treesitter/nvim-treesitter' },
+    event = 'BufReadPost', -- Load after opening a buffer
+  },
+  {
+    'mistweaverco/kulala.nvim',
+    ft = { 'http', 'rest' },
+    opts = {
+      -- your configuration comes here
+      global_keymaps = true,
+    },
+  },
+  {
+    'iamcco/markdown-preview.nvim',
+    cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
+    build = 'cd app && npm install',
+    init = function()
+      vim.g.mkdp_filetypes = { 'markdown' }
+    end,
+    ft = { 'markdown' },
+  },
+}
