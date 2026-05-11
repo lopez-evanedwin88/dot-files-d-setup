caa63ebcbe91e1f8b9f93b6fabc7d1799f04c41d
Evan Edwin Lopez <lopez.evanedwin@gmail.com>
2026-05-11 13:54:42 +0800

    :wrench: Added breadcrumbs

--- .config/nvim/lua/custom/plugins/lspsaga.lua
+++ .config/nvim/lua/custom/plugins/lspsaga.lua
@@ -1,11 +1,27 @@
 return {
-  'kevinhwang91/nvim-bqf',
-  ft = 'qf', -- Lazy load on quickfix window open
-  opts = {
-    auto_enable = true,
-    preview = {
-      win_height = 12,
-      border = 'rounded',
+  {
+    'kevinhwang91/nvim-bqf',
+    ft = 'qf', -- Lazy load on quickfix window open
+    opts = {
+      auto_enable = true,
+      preview = {
+        win_height = 12,
+        border = 'rounded',
+      },
     },
-  }
+  },
+  {
+    'Bekaboo/dropbar.nvim',
+    -- optional, but required for fuzzy finder support
+    dependencies = {
+      'nvim-telescope/telescope-fzf-native.nvim',
+      build = 'make',
+    },
+    config = function()
+      local dropbar_api = require 'dropbar.api'
+      vim.keymap.set('n', '<Leader>;', dropbar_api.pick, { desc = 'Pick symbols in winbar' })
+      vim.keymap.set('n', '[;', dropbar_api.goto_context_start, { desc = 'Go to start of current context' })
+      vim.keymap.set('n', '];', dropbar_api.select_next_context, { desc = 'Select next context' })
+    end,
+  },
 }
